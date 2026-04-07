e009bb12373124ac43372a25f5a3123e0aa05bdc
jayden <46307433+kermeow@users.noreply.github.com>
2026-04-08 00:35:58 +0100

    static link ssp

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package opus
 /*
 // standard compilation flags
 #cgo CFLAGS: -DHAVE_CONFIG_H -D_FORTIFY_SOURCE=2 -DPIC -fvisibility=hidden -fstack-protector-strong -fno-common
-#cgo windows LDFLAGS: -lssp
+#cgo windows LDFLAGS: -lssp_nonshared
 
 // add includes needed for opus, including building the actual opus lib
 #cgo CFLAGS: -Iopus-1.5.2 -Iopus-1.5.2/include -Iopus-1.5.2/celt -Iopus-1.5.2/silk -Iopus-1.5.2/silk/float
